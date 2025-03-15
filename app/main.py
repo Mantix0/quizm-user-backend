38a137b8ccb878c33b778aefdac4777d61e18d9d
Mantix <89809041+Mantix0@users.noreply.github.com>
2025-03-15 16:29:44 +0300

    enable CORS

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi.encoders import jsonable_encoder
+from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import JSONResponse
 
 from .users.router import router as router_users, router_records
@@ -11,6 +12,16 @@ from .database import *
 app = FastAPI()
 app.include_router(router_users)
 app.include_router(router_records)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.exception_handler(404)
